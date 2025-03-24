531b4cf152817cac4b2bb98fb350147f78a5e86b
Rahul Mishra <rm983845@gmail.com>
2025-03-24 10:37:25 +0530

    Update fsi.h

--- fsi.h
+++ fsi.h
@@ -9,19 +9,4 @@ void fsi_write_and_advance();
 void fsi_grid_motion(Domain* domain, Dynamic_Thread* dt, real time, real dtime);
 void fsi_plot_coords();
 
-
-// Edited
-// #include <stdbool.h>
-// int precicec_getMeshID(const char* meshName);
-// int precicec_getDataID(const char* dataName, int meshID);
-// void precicec_readBlockVectorData(int dataID, int size, int* vertexIDs, double* values);
-// void precicec_writeBlockVectorData(int dataID, int size, int* vertexIDs, double* values);
-// int precicec_actionWriteIterationCheckpoint();
-// int precicec_actionReadIterationCheckpoint();
-// bool precicec_isActionRequired(int action);
-// void precicec_markActionFulfilled(int action);
-// bool precicec_isWriteDataRequired(double computedTimeStepSize);
-// void precicec_createSolverInterface(const char* solverName, const char* configFileName, int solverProcessIndex, int solverProcessSize);
-
-
 #endif /* FSI_H_ */
